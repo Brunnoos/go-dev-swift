2152e2f1798c89ad2ee908792b86d0660d3bca2e
BrunoOS <brunno.os159@gmail.com>
2022-03-09 20:14:50 -0300

    Improvements to Class 02 Challange

--- Aula 02/Aula02 - Desafio Playground.playground/Contents.swift
+++ Aula 02/Aula02 - Desafio Playground.playground/Contents.swift
@@ -27,7 +27,7 @@ class Stack<T: Any> {
     }
     
     func pop() -> T? {
-        if top > 0 {
+        if top >= 0 {
             print("Popping top element.")
             top -= 1
             return elems[top + 1]
@@ -50,16 +50,19 @@ class Stack<T: Any> {
 var newStack = Stack<String>(5)
 
 print("Is the Stack empty? \(newStack.isEmpty())")
+newStack.pop()
 newStack.push(element: "Test 1")
+print("Stack top element was: \(newStack.pop() ?? "nil")")
 newStack.push(element: "Test 2")
 newStack.push(element: "Test 3")
+newStack.push(element: "Test 4")
 print("Is the Stack full? \(newStack.isFull())")
 print("Is the Stack empty? \(newStack.isEmpty())")
-newStack.push(element: "Test 4")
 newStack.push(element: "Test 5")
-print("Is the Stack full? \(newStack.isFull())")
 newStack.push(element: "Test 6")
-print("Stack top element was: \(newStack.pop() ?? "Empty")")
+print("Is the Stack full? \(newStack.isFull())")
 newStack.push(element: "Teste 7")
+print("Stack top element was: \(newStack.pop() ?? "nil")")
+newStack.push(element: "Teste 8")
 // newStack.push(element: 9) <-- Throws an error
 
